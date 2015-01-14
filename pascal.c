649c6f719cbd4139c82d09005ee958207f9a687e
Suyash <suyash.bansal5@gmail.com>
2015-01-14 18:36:27 +0530

    minor update

--- pascal.c
+++ pascal.c
@@ -1,36 +1,41 @@
 #include<stdio.h>
 
-int pascal(int *);
+int pascal(int *,int);
 int fact(int);
 
 int main()
 {
-    int n,t,i,array[100],res[100];
+    int n,t,i,j,array[100],res[100];
     scanf("%d",&n);
     scanf("%d",&t);
     for(i=0; i<t; i++)
     {
         scanf(" %[^\n]d",array);
-        res[i]=pascal(array);
+        //res[i]=pascal(array,n); // <-- error !!!
         /*for(j=0; j<10; j++)
         {
             scanf("%d",&array[j]);
         }*/
     }
-    printf("\nYou entered: %s", array);
-    printf("\nYou entered: %s", res);
+    //printf("\nYou entered: %s", array);
+    printf("%s", res);
     return 0;
 }
 
-int pascal(int *ptr_2_array)
+int pascal(int *ptr_2_array,int n)
 {
     int sum=0,r;
     //sum = sum + (fact(n)/(fact(n-r)*fact(r)));
-    for(r=1; r<100; r++)
+    if(n<=ptr_2_array[0])
     {
-        sum = sum + (fact(ptr_2_array[0])/(fact(ptr_2_array[0]-ptr_2_array[r])*fact(ptr_2_array[r])));
+        for(r=1; r<100; r++)
+        {
+            sum = sum + (fact(ptr_2_array[0])/(fact(ptr_2_array[0]-ptr_2_array[r])*fact(ptr_2_array[r])));
+        }
+        return sum;
     }
-    return sum;
+    else
+        return -1;
 }
 
 int fact(int x)
