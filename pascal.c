c54000185b836ec1761cba405c357fac73b17d29
Suyash <suyash.bansal5@gmail.com>
2015-01-15 18:03:18 +0530

    input example added

--- pascal.c
+++ pascal.c
@@ -8,34 +8,47 @@ int main()
     int n,t,i,j,array[100],res[100];
     scanf("%d",&n);
     scanf("%d",&t);
-    for(i=0; i<t; i++)
+    for(i=0; i<2; i++)
     {
-        scanf(" %[^\n]d",array);
-        //res[i]=pascal(array,n); // <-- error !!!
-        /*for(j=0; j<10; j++)
+        //scanf(" %[^\n]d",array[j]);
+        //printf("%d ",array[0]);
+        //res[i]=pascal(array,n);
+
+        for(j=0; j<10; j++)
         {
             scanf("%d",&array[j]);
-        }*/
+            if(array[j] == '\0'){
+                break;
+            }
+        }
     }
-    //printf("\nYou entered: %s", array);
     printf("%s", res);
     return 0;
 }
 
-int pascal(int *ptr_2_array,int n)
+int pascal(int *ptr_2_array,int c)
 {
-    int sum=0,r;
+    int sum=0,i;
+    int n=ptr_2_array[0];
     //sum = sum + (fact(n)/(fact(n-r)*fact(r)));
-    if(n<=ptr_2_array[0])
+    if(c>=n)
     {
-        for(r=1; r<100; r++)
+
+        for(i=1; i<3; i++)
         {
-            sum = sum + (fact(ptr_2_array[0])/(fact(ptr_2_array[0]-ptr_2_array[r])*fact(ptr_2_array[r])));
+            int r = ptr_2_array[i];
+            //sum = sum + (fact(ptr_2_array[0])/(fact(ptr_2_array[0]-ptr_2_array[r])*fact(ptr_2_array[r])));
+            //sum = sum + (fact(n)/(fact(n-r)*fact(r)));
         }
+        printf("%d ", n);
+        printf("if pascal");
         return sum;
     }
     else
+    {
+        printf("Else pascal");
         return -1;
+    }
 }
 
 int fact(int x)
